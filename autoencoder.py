1c8e4e2b18c258518bea611b0e6cefdb55fdd76e
Tavernier Antoine <tavernier.antoine@hotmail.fr>
2021-05-26 16:49:07 +0200

    tpu

--- autoencoder.py
+++ autoencoder.py
@@ -4,8 +4,7 @@ from utils import load_images, load_mnist, load_from_numpy, load_mnist_PT
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
-import torch_xla
-import torch_xla.core.xla_model as xm
+import tqdm
 
 
 class AutoEncoder:
@@ -16,7 +15,10 @@ class AutoEncoder:
         #     nn.Flatten(),
         #     [nn.Linear(m / i, (m / i) * 2 ) for i  in range(1, n)]
         # )
-        self.dev = xm.xla_device()
+        if torch.cuda.is_available():  
+            self.dev = "cuda:0" 
+        else:  
+            self.dev = "cpu"  
         self.shape = next(iter(X))[0].size()[1]
         self.encode_ = nn.Sequential(
             nn.Linear(self.shape, 32),
@@ -27,7 +29,7 @@ class AutoEncoder:
             nn.Sigmoid(),
             nn.Linear(8, 4),
             nn.Sigmoid(),
-            nn.Linear(4, 2)
+            nn.Linear(4, 2),
         )
 
         self.decode_ = nn.Sequential(
@@ -39,23 +41,19 @@ class AutoEncoder:
             nn.Sigmoid(),
             nn.Linear(16, 32),
             nn.Sigmoid(),
-            nn.Linear(32, self.shape)
+            nn.Linear(32, self.shape),
         )
         # self.decode = nn.Sequential(
         #     [nn.Linear(m / i, (m / i) * 2 ) for i  in range(n, 1, -1)]
         # )
 
-        
         self.model_ = nn.Sequential(self.encode_, self.decode_).to(self.dev)
 
     def fit(self, epochs, lr):
         criterion = nn.MSELoss()
-        optimizer = torch.optim.Adam(
-            self.model_.parameters(), lr=lr, weight_decay=1e-5
-        )
+        optimizer = torch.optim.Adam(self.model_.parameters(), lr=lr, weight_decay=1e-5)
         outputs = []
-        for epoch in range(epochs):
-            print(epoch)
+        for epoch in tqdm.tqdm(range(epochs)):
             for data in self.X:
                 x, _ = data
                 x = x.to(self.dev)
@@ -68,14 +66,12 @@ class AutoEncoder:
     def encode(self, X):
         return self.encode_(X)
 
-
     def decode(self, X):
         return self.decode_(X)
 
 
-# if __name__ == "__main__":
-#     train_loader = load_mnist_PT()
+if __name__ == "__main__":
+    train_loader = load_mnist_PT()
 
-#     model = AutoEncoder(4, 32, train_loader)
-#     model.fit(2, 0.01)
-
+    model = AutoEncoder(4, 32, train_loader)
+    model.fit(100, 0.01)
