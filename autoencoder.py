e56b69f9407f799d9f6d2c9ea47e7d252ddb84c3
nicolas ROCHE <nico_roche@hotmail.fr>
2021-05-27 09:10:26 +0200

    last activation

--- autoencoder.py
+++ autoencoder.py
@@ -8,29 +8,32 @@ import tqdm
 
 
 class AutoEncoder:
-    def __init__(self, layers, X: np.ndarray, activation):
+    def __init__(self, layers, X: np.ndarray, activation, last_activation_encode=None, last_activation_decode=None):
         torch.manual_seed(42)
         self.dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.X = X
 
         self.shape = next(iter(X))[0].size()[1]
 
-        self.init_encode_model(layers, activation)
-        self.init_decode_model(layers, activation)
+        self.init_encode_model(layers, activation, last_activation_encode)
+        self.init_decode_model(layers, activation, last_activation_decode)
         self.model_ = nn.Sequential(self.encode_, self.decode_).to(self.dev)
 
-    def init_encode_model(self, layers, activation):
+    def init_encode_model(self, layers, activation, last_activation):
         encode_layers = []
         encode_layers.append(nn.Linear(self.shape, layers[0]))
         encode_layers.append(activation)
         for i, l in enumerate(layers):
-            if i == len(layers) - 1:
+            if i == len(layers) - 2:
                 break
             encode_layers.append(nn.Linear(l, layers[i + 1]))
             encode_layers.append(activation)
+        encode_layers.append(nn.Linear(l, layers[i + 1]))
+        if last_activation:
+            encode_layers.append(last_activation)
         self.encode_ = nn.Sequential(*encode_layers)
 
-    def init_decode_model(self, layers, activation):
+    def init_decode_model(self, layers, activation, last_activation):
         decode_layers = []
         reverse_layer = layers[::-1]
         for i, l in enumerate(reverse_layer):
@@ -39,7 +42,8 @@ class AutoEncoder:
             decode_layers.append(nn.Linear(l, reverse_layer[i + 1]))
             decode_layers.append(activation)
         decode_layers.append(nn.Linear(reverse_layer[-1], self.shape))
-        decode_layers.append(activation)
+        if last_activation:
+            decode_layers.append(last_activation)
         self.decode_ = nn.Sequential(*decode_layers)
 
     def fit(self, epochs, lr):
