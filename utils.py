f28219785ded94065001bd5d5a9e5b4039633223
Antoine Tavernier <tavernier.antoine@hotmail.fr>
2021-05-28 13:49:48 +0200

    black utils.py

--- utils.py
+++ utils.py
@@ -10,7 +10,6 @@ from torch.utils.data.dataset import Dataset
 import tensorflow as tf
 
 
-
 def load_images(path, image_width, image_height, save=False):
 
     images = glob.glob(path + "/*/*.jpg")
@@ -37,33 +36,51 @@ def load_images(path, image_width, image_height, save=False):
 def load_from_numpy():
     return np.load("X.npy"), np.load("Y.npy")
 
+
 def load_mnist_PT():
-    mnist_data = datasets.MNIST(".", True, download=True, transform=transforms.Compose([
-                                transforms.ToTensor(),
-                                    transforms.Lambda(lambda x: torch.flatten(x))
-                                ]))
-    train_loader = torch.utils.data.DataLoader(mnist_data, 
-                                            batch_size=64, 
-                                            shuffle=True)
+    mnist_data = datasets.MNIST(
+        ".",
+        True,
+        download=True,
+        transform=transforms.Compose(
+            [transforms.ToTensor(), transforms.Lambda(lambda x: torch.flatten(x))]
+        ),
+    )
+    train_loader = torch.utils.data.DataLoader(mnist_data, batch_size=64, shuffle=True)
     return train_loader
 
+
 def load_mnist_CNN_PT(batch_size):
-    mnist_data = datasets.MNIST(".", True, download=True, transform=transforms.Compose([
-                                transforms.ToTensor(),
-                                ]))
-    train_loader = torch.utils.data.DataLoader(mnist_data, 
-                                            batch_size=batch_size, 
-                                            shuffle=True)
+    mnist_data = datasets.MNIST(
+        ".",
+        True,
+        download=True,
+        transform=transforms.Compose(
+            [
+                transforms.ToTensor(),
+            ]
+        ),
+    )
+    train_loader = torch.utils.data.DataLoader(
+        mnist_data, batch_size=batch_size, shuffle=True
+    )
     return train_loader
+
+
 def load_mnist():
     (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
-    x_train = np.reshape(x_train, (np.shape(x_train)[0], np.shape(x_train)[1] * np.shape(x_train)[2]))
-    x_train  = x_train / 255.0
+    x_train = np.reshape(
+        x_train, (np.shape(x_train)[0], np.shape(x_train)[1] * np.shape(x_train)[2])
+    )
+    x_train = x_train / 255.0
     return x_train, y_train
+
 
 def load_mnist2():
     (train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
-    train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+    train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype(
+        "float32"
+    )
     train_images = (train_images - 127.5) / 127.5  # Normalize the images to [-1, 1]
     return tf.data.Dataset.from_tensor_slices(train_images).shuffle(60000).batch(256)
 
@@ -79,7 +96,9 @@ class CustomDatasetFromFile(Dataset):
         """
         # Get image list
         self.image_list = glob.glob(folder_path + "/*/*.jpg")
-        self.image_list_label = np.array(["\\".join(img.split("\\")[-2:-1]) for img in self.image_list])
+        self.image_list_label = np.array(
+            ["\\".join(img.split("\\")[-2:-1]) for img in self.image_list]
+        )
         # Calculate len
         self.data_len = len(self.image_list)
 
@@ -89,7 +108,7 @@ class CustomDatasetFromFile(Dataset):
         # Open image
         im_as_im = Image.open(single_image_path)
 
-        im_as_np = np.asarray(im_as_im)/255
+        im_as_np = np.asarray(im_as_im) / 255
 
         im_as_np = np.expand_dims(im_as_np, 0)
 
@@ -101,6 +120,7 @@ class CustomDatasetFromFile(Dataset):
 
     def __len__(self):
         return self.data_len
+
 
 if __name__ == "__main__":
     # Xload_images("./simpsons_dataset", 100, 100, True), Y =
